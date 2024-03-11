e91c1ce5fac0e368136fdc79366f3f44c9f38adb
Noah <na933950@gmail.com>
2024-03-11 13:10:41 -0700

    Changes by meeting

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -16,17 +16,17 @@ interface Service {
 const Services = () => {
   const services: Service[] = [
     {
-      icon: <FaBriefcase />,
-      title: "Bar Exam Coaching",
-      text: "There are many excellent bar preparation courses, and everyone should take them- but no one will know how to help you like a one-on-one coach. I will coach you through the challenges you face and help you make decisions by guiding your practice efforts, providing feedback and helping you to visualize your goals. Let’s work together to get you across the finish line.",
+      icon: <FaBuildingColumns />,
+      title: "Law School Application Coaching",
+      text: "With thousands of law schools, only a few will be the right fit for you. I can help you identify the right ones for you, and help you present your best application. I will work with you to create a personalized plan to help you identify your priorities.",
       amenities: [
-        "Custom designed plan for studying",
+        "School selection advising",
+        "Essay and application review",
+        "Application timeline and plan development",
+        "Priority identification",
         "One on one's with an experienced coach",
-        "Personalized feedback",
-        "Practice exams",
-        "Question walkthroughs",
       ],
-      price: "$300",
+      price: "$400",
     },
     {
       icon: <IoSchoolSharp />,
@@ -42,17 +42,17 @@ const Services = () => {
       price: "$500",
     },
     {
-      icon: <FaBuildingColumns />,
-      title: "Law School Application Coaching",
-      text: "With thousands of law schools, only a few will be the right fit for you. I can help you identify the right ones for you, and help you present your best application. I will work with you to create a personalized plan to help you identify your priorities.",
+      icon: <FaBriefcase />,
+      title: "Bar Exam Coaching",
+      text: "There are many excellent bar preparation courses, and everyone should take them- but no one will know how to help you like a one-on-one coach. I will coach you through the challenges you face and help you make decisions by guiding your practice efforts, providing feedback and helping you to visualize your goals. Let’s work together to get you across the finish line.",
       amenities: [
-        "School selection advising",
-        "Essay and application review",
-        "Application timeline and plan development",
-        "Priority identification",
+        "Custom designed plan for studying",
         "One on one's with an experienced coach",
+        "Personalized feedback",
+        "Practice exams",
+        "Question walkthroughs",
       ],
-      price: "$400",
+      price: "$300",
     },
   ];
   return (
