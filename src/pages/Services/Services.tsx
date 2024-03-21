02d68e4f5435f88863784684a9616ac476793e36
Noah <na933950@gmail.com>
2024-03-21 13:24:36 -0700

    Changes

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -4,7 +4,7 @@ import { FaBriefcase } from "react-icons/fa6";
 import { IoSchoolSharp } from "react-icons/io5";
 import { FaBuildingColumns } from "react-icons/fa6";
 import Fade from "../../assets/Fade";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 
 interface Service {
   icon: React.ReactNode;
@@ -15,72 +15,49 @@ interface Service {
 }
 
 const Services = () => {
-  const [services, setServices] = useState<Service[]>([
+  const services: Service[] = [
     {
       icon: <FaBuildingColumns />,
-      title: "Law School Application Coaching",
+      title: "Choosing a Career in Law or Advocacy",
       text: "With thousands of law schools, only a few will be the right fit for you. I can help you identify the right ones for you, and help you present your best application. I will work with you to create a personalized plan to help you identify your priorities.",
       amenities: [
-        "School selection advising",
-        "Essay and application review",
+        "Law School or Another Path?",
+        "Grad School selection strategy",
         "Application timeline and plan development",
-        "Priority identification",
-        "One on one's with an experienced coach",
+        "Communicating and meeting with admissions professionals",
+        "Best communication practices with faculty",
       ],
-      price: "$400",
+      price: "$150/hour",
     },
     {
       icon: <IoSchoolSharp />,
-      title: "Law School Advising",
+      title: "Law School Support",
       text: "The US Law School experience is unique, help get prepared for choosing classes, studying for them and passing your exams. Together, we will develop a strategy that plays to your strengths.",
       amenities: [
         "Class selection advising",
-        "One on one's with an experienced coach",
-        "Study plan development",
-        "Best practices for class",
-        "Content review",
+        "Balancing bar exam courses with career practice interests",
+        "Thesis topic selection",
+        "Best practices for study and class preparation",
+        "One on One coaching for 1L exam preparation",
       ],
-      price: "$500",
+      price: "$150/hour",
     },
     {
       icon: <FaBriefcase />,
       title: "Bar Exam Coaching",
       text: "There are many excellent bar preparation courses, and everyone should take them- but no one will know how to help you like a one-on-one coach. I will coach you through the challenges you face and help you make decisions by guiding your practice efforts, providing feedback and helping you to visualize your goals. Let’s work together to get you across the finish line.",
       amenities: [
-        "Custom designed plan for studying",
-        "One on one's with an experienced coach",
-        "Personalized feedback",
-        "Practice exams",
-        "Question walkthroughs",
+        "Custom designed strategy: which jurisdiction and when",
+        "Custom designed study plan that fits your life",
+        "One on one coaching and skills development",
+        "Personalized feedback on your study performance",
+        "Practice simulations that reinforce knowledge and short term memory",
       ],
-      price: "$300",
+      price: "$150/hour",
     },
-  ]);
-
-  // Setting all titles to the same length so it sizes things the same in cards...
-  useEffect(() => {
-    let maxLen = 0;
-    services.forEach((service) => {
-      if (service.title.length > maxLen) {
-        maxLen = service.title.length;
-      }
-    });
-    const newServices: Service[] = [];
-    services.forEach((service) => {
-      let diffLen = maxLen - service.title.length;
-      let extraSpaces = "";
-      for (let i = 0; i < diffLen; i++) {
-        extraSpaces += " ";
-      }
-      let newService = { ...service };
-      newService.title += extraSpaces;
-      newServices.push(newService);
-    });
-    setServices(newServices);
-  }, []);
+  ];
 
   const serviceContainerRef = useRef<HTMLDivElement>(null);
-
 
   return (
     <div className={styles.container}>
